aef471a3393daed8d41aa9335d61d6207e189922
PeriniDev <117356668+engperini@users.noreply.github.com>
2024-11-24 00:42:07 -0300

    Add files via upload

---
+++ static/js/register.js
@@ -0,0 +1,67 @@
+
+
+let auth;
+
+async function loadFirebaseConfig() {
+try {
+    const response = await fetch('/api/firebase-config');
+    const config = await response.json();
+    firebase.initializeApp(config);
+    console.log("Firebase loaded");
+
+    // Inicialize auth após carregar o Firebase
+    auth = firebase.auth();
+} catch (error) {
+    console.error("Failed to load Firebase config:", error);
+}
+}
+
+loadFirebaseConfig();
+
+function login() {
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+    const loginError = document.getElementById("login-error");
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then(userCredential => {
+        // Signed in 
+        const user = userCredential.user;
+        // Redirect to app or display welcome message
+        
+        window.location.href = "/app"; 
+    })
+    .catch(error => {
+        loginError.textContent = error.message;
+    });
+}
+
+function register() {
+    const email = document.getElementById("register-email").value;
+    const password = document.getElementById("register-password").value;
+    const registerError = document.getElementById("register-error");
+
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(userCredential => {
+        // Signed in 
+        const user = userCredential.user;
+        // Redirect to app or display welcome message
+        
+        window.location.href = "/app"; 
+    })
+    .catch(error => {
+        registerError.textContent = error.message;
+    });
+}
+
+function toggleForm(form) {
+    if (form === 'register') {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("register-form").style.display = "block";
+    } else {
+    document.getElementById("login-form").style.display = "block";
+    document.getElementById("register-form").style.display = "none";
+    }
+}
+
+
